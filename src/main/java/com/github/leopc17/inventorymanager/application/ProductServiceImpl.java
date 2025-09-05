a36d5ec612aac518b678096ec394f3c4470ee45b
Florido-hub <floridodinizz@gmail.com>
2025-09-05 17:42:43 -0300

    feat: Product Service Impl

--- src/main/java/com/github/leopc17/inventorymanager/application/ProductServiceImpl.java
+++ src/main/java/com/github/leopc17/inventorymanager/application/ProductServiceImpl.java
@@ -2,33 +2,45 @@ package com.github.leopc17.inventorymanager.application;
 
 import com.github.leopc17.inventorymanager.domain.entity.Product;
 import com.github.leopc17.inventorymanager.domain.input.ProductServicePort;
+import com.github.leopc17.inventorymanager.domain.output.ProductRepositoryPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class ProductServiceImpl implements ProductServicePort {
+
+    private final ProductRepositoryPort productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepositoryPort productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product createProduct(Product product) {
-        return null;
+        return productRepository.createProduct(product);
     }
 
     @Override
     public List<Product> getAllProduct() {
-        return null;
+        return productRepository.getAllProduct();
     }
 
     @Override
     public Product getById(UUID id) {
-        return null;
+        return productRepository.getById(id);
     }
 
     @Override
     public void updateProduct(Product product, UUID id) {
-
+        productRepository.updateProduct(product,id);
     }
 
     @Override
     public void deleteById(UUID id) {
-
+        productRepository.deleteById(id);
     }
 }
