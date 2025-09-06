8e1915d5ab9ef83546e280c14ac79820374de997
leopc17 <LeonardoEAF45@gmail.com>
2025-09-06 10:18:02 -0300

    refactor(productEntity): refatora a classe para herdar classe Produto sobreescrevendo os métodos existentes

--- src/main/java/com/github/leopc17/inventorymanager/infrastructure/entity/ProductEntity.java
+++ src/main/java/com/github/leopc17/inventorymanager/infrastructure/entity/ProductEntity.java
@@ -1,5 +1,6 @@
 package com.github.leopc17.inventorymanager.infrastructure.entity;
 
+import com.github.leopc17.inventorymanager.domain.entity.Product;
 import com.github.leopc17.inventorymanager.domain.enums.ProductCategory;
 import jakarta.persistence.*;
 
@@ -8,86 +9,51 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "tb_product")
-public class ProductEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
-    private BigDecimal price;
-
-    @Column(columnDefinition = "TEXT",nullable = false)
-    private String shortDescription;
-
-    @Column(columnDefinition = "TEXT",nullable = false)
-    private String longDescription;
-
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private ProductCategory category;
+public class ProductEntity extends Product {
 
     public ProductEntity() {
+        super();
     }
 
     public ProductEntity(UUID id, String name, BigDecimal price, String longDescription, String shortDescription, ProductCategory category) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.longDescription = longDescription;
-        this.shortDescription = shortDescription;
-        this.category = category;
+        super(id, name, price, longDescription, shortDescription, category);
     }
 
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public UUID getId() {
-        return id;
+        return super.getId();
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
+    @Column(nullable = false)
+    @Override
     public String getName() {
-        return name;
+        return super.getName();
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    @Column(nullable = false)
+    @Override
     public BigDecimal getPrice() {
-        return price;
+        return super.getPrice();
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
+    @Column(columnDefinition = "TEXT",nullable = false)
+    @Override
     public String getLongDescription() {
-        return longDescription;
+        return super.getLongDescription();
     }
 
-    public void setLongDescription(String longDescription) {
-        this.longDescription = longDescription;
-    }
-
+    @Column(columnDefinition = "TEXT",nullable = false)
+    @Override
     public String getShortDescription() {
-        return shortDescription;
+        return super.getShortDescription();
     }
 
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Override
     public ProductCategory getCategory() {
-        return category;
-    }
-
-    public void setCategory(ProductCategory category) {
-        this.category = category;
+        return super.getCategory();
     }
 }
